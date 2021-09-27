70cc9afb062ec86cfa3aa25e64d22f4f48c33f2b
lizchia <midory0cc@hotmail.com>
2021-09-27 21:06:17 +0800

    add react frame woork and light box structure

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="upbox">
+          <div className="light-box box1"></div>
+          <div className="light-box box2"></div>
+          <div className="light-box box3"></div>
+        </div>
+        <div className="midbox">
+          <div className="light-box box4"></div>
+          <div className="light-box box5"></div>
+          <div className="light-box box6"></div>
+        </div>
+        <div className="downbox">
+          <div className="light-box box7"></div>
+          <div className="light-box box8"></div>
+          <div className="light-box box9"></div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
